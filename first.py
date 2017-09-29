a4b9ae54fdc5ceb0ab7e141d7ecea973575e4279
chock <zhangdong_125@126.com>
2017-09-29 21:43:59 +0800

    modified:   first.py

--- first.py
+++ first.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python
 
-def add(a, b):
-	return a + b
+import socket
 
-a = 10
-b = 13
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print add(a, b)
+s.bind(socket.INADDR_ANY, 8888)
+
+s.listen(10)
+
+while True:
+        sock, addr = s.accept()
+        t = threading.Thread(target = tcplink, args = (sock, addr))
+        t.start()
+
+def tcplink(sock, addr)
+        print("Accept new connection from %s:%s..." % addr)
+        sock.send(b'Welcome!')
+        while True:
+                data = sock.recv(1024)
+                time.sleep(1)
+                if not data or data.decode('utf-8') == 'exit':
+                        break
+                sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+        sock.close()
+        print('Connection from %s:%s closed.' %s addr)
+        
+
 
 
